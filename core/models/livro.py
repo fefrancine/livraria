1a422c779192f3c7f766b3e52614dbd17b011d9f
Fefrancine <fernanda.francine.f@gmail.com>
2025-04-22 08:38:54 -0300

    feat: inclusão do relacionamento n para n entre Livro e Autor

--- core/models/livro.py
+++ core/models/livro.py
@@ -9,12 +9,12 @@ class Livro(models.Model):
     isbn = models.CharField(max_length=32, null=True, blank=True)
     quantidade = models.IntegerField(default=0, null=True, blank=True)
     preco = models.DecimalField(max_digits=7, decimal_places=2, default=0, null=True, blank=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name='livros', null=True, blank=True)
+    editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name="livros", null=True, blank=True)
+    # autor = models.ForeignKey('Autor', on_delete=models.PROTECT, related_name='livros', null=True, blank=True)
+    # coautor = models.ForeignKey('Autor', on_delete=models.PROTECT, related_name='livros_coautor', null=True, blank=True)
+    autores = models.ManyToManyField('Autor', related_name='livros', blank=True) 
 
 
-categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name='livros', null=True, blank=True)
-editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name="livros", null=True, blank=True)
-
-
-
-def __str__(self):
-    return f'({self.id}) {self.titulo} ({self.quantidade})'
+    def __str__(self):
+        return f'({self.id}) {self.titulo} ({self.quantidade})'
